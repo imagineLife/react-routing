194915a04e4a3b9675b7897c37615e01c7328b0e
Eric Laursen <ericjlaursen@gmail.com>
2022-12-27 10:33:57 -0500

    more description

--- src/routes/itemById.js
+++ src/routes/itemById.js
@@ -7,10 +7,20 @@ function ItemById() {
   return (
     <div>
       <h2>Item By Id Page</h2>
-      <p>This is for item id {itemId}.</p>
-      <p>The item number comes from <code style={{ writeSpace: "pre" }}>{`const { itemId } = useParams()`}</code> functionality provided by react-router-dom.</p>
+      <p>
+        The item number comes from{' '}
+        <code style={{ writeSpace: 'pre' }}>{`const { itemId } = useParams()`}</code> functionality
+        provided by react-router-dom.
+      </p>
+      <p>
+        This leverages the url parameter feature in react-router-dom: the{' '}
+        <code style={{ whiteSpace: 'pre' }}>{'const { itemId } = useParams()'}</code> detail, where
+        "useParams" can return url params for use in the route. Here, I use the param to show the
+        item id in the page.
+      </p>
+      <p>The "useParams" hook here gets the item id from the url as {itemId }. The unique detail about this "itemId" param is that this webpage, itself, does not have <b>any unique content</b>, other than the value of that itemId. React-route-dom does the "work" of pulling the itemId from the url.</p>
     </div>
-  )
+  );
 }
 
 export default ItemById
--- src/routes/items.js
+++ src/routes/items.js
@@ -28,8 +28,32 @@ function Items() {
     <>
       <h2>Items</h2>
       <p>A handful of items.</p>
+
+      <h3>2 New Routes: A List route and a "by-id" route</h3>
+      <h4>The List Route</h4>
+      <p>
+        The First route to add is the "list" route. This route shows a list of items. Each item has
+        text to display about the item as well as an item "id" - hopefully a reasonably regular task
+        to do in a SaaS product.
+      </p>
+      <p>
+        Each item in the list route is also a link to another url: the{' '}
+        <code style={{ whiteSpace: 'pre' }}>{'/items/:itemId'}</code> route. OnClick of each item
+        the browser redirects to that url with the item id as the url param: item id 123 will
+        navigate to <code style={{ whiteSpace: 'pre' }}>{'/items/123'}</code>.
+      </p>
+
+      <h4>The Item-by-id Route</h4>
+      <p>The second route here is particular to a specific item, where the item.</p>
+      <p>
+        This leverages the url parameter feature in react-router-dom: the{' '}
+        <code style={{ whiteSpace: 'pre' }}>{'const { itemId } = useParams()'}</code> detail, where "useParams" can return url params for use in the route. Here, I use the param to show the item id in the page. 
+      </p>
+
       <ul>
-        {items.map(d => <Item key={d.name} d={d} />)}
+        {items.map((d) => (
+          <Item key={d.name} d={d} />
+        ))}
       </ul>
     </>
   );
