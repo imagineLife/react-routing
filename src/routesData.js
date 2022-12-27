7b4c03a521659c6408936168fa52dcffe11ca18e
Eric Laursen <ericjlaursen@gmail.com>
2022-12-27 11:16:29 -0500

    move routes data and component+arr to unique file

---
+++ src/routesData.js
@@ -0,0 +1,37 @@
+import React, { lazy } from 'react';
+
+const Home = lazy(() => import('./routes/home'));
+const WithNav = lazy(() => import('./routes/withNav'));
+const Items = lazy(() => import('./routes/items'));
+const ItemById = lazy(() => import('./routes/itemById'));
+const SpecificItem = lazy(() => import('./routes/itemSpecific'));
+const routesArr = [
+  {
+    p: '/',
+    e: Home,
+    s: 'Starting',
+  },
+  {
+    p: '/with-nav',
+    e: WithNav,
+    s: 'With A Nav',
+  },
+  {
+    p: '/items',
+    e: Items,
+    s: 'Items',
+  },
+  {
+    // NOTE: itemId matches the useParams param in the ItemById component
+    p: '/items/:itemId',
+    e: ItemById,
+    s: 'Item By Id',
+  },
+  {
+    p: '/items/a-unique-url',
+    e: SpecificItem,
+    s: 'Specific Item',
+  },
+];
+
+export default routesArr;
